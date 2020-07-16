70cbbe8bcb635bbd262c8641a12074ab56c2a53c
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-07-16 14:00:09 +0200

    initial server setup in app.js

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 require('dotenv').config()
+
+// Dependencies
+const express = require('espress');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+// Routes
+
+// Starting mongoDB
+mongoose.connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Middlewares
+app.use(express.static('public'));
+app.use(bodyParse.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors({
+    credentials: true,
+}));
+
+const app = express();
+
+// Use Routes
+
+/** Creates and passes a 404 error if the route is not a register route*/ 
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+/** Catces errors and returns a message */
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+/** Starts the server */
+app.listen(process.env.PORT, () => {
+    console.log("Server started");
+});
